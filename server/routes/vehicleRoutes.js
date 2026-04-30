ee57878e60da92ca4f5f667a20b1033cbb33e352
Ishad-Shyamal <ishadss2016@gmail.com>
2026-05-01 03:52:51 +0530

    add routes to backend

---
+++ server/routes/vehicleRoutes.js
@@ -0,0 +1,28 @@
+//server/routes/vehicleRoutes.js
+const express = require('express')
+const router = express.Router()
+
+const {
+    createVehicle,
+    getAllVehicles,
+    getVehicleById,
+    updateVehicle,
+    deleteVehicle
+} = require('../controllers/vehicleController')
+
+// Create vehicle
+router.post('/', createVehicle)
+
+// Get all vehicles
+router.get('/', getAllVehicles)
+
+// Get vehicle by ID
+router.get('/:id', getVehicleById)
+
+// Update vehicle
+router.put('/:id', updateVehicle)
+
+// Delete vehicle
+router.delete('/:id', deleteVehicle)
+
+module.exports = router
